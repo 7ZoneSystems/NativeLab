84183bd512c059ba1610f24fbef2018746db2c4d
hrishi <hrirakes@gmail.com>
2026-03-08 16:04:50 +0530

    added automated commit

---
+++ comt.sh
@@ -0,0 +1,18 @@
+echo "----------------------------------"
+echo " NativeLab Git Auto Push Script"
+echo "----------------------------------"
+read -p "Enter commit name: " msg
+
+if [ -z "$msg" ]; then
+    echo "Commit name cannot be empty!"
+    exit 1
+fi
+echo ""
+echo "Adding files..."
+git add .
+echo "Creating commit..."
+git commit -m "$msg"
+echo "Pushing to GitHub..."
+git push
+echo ""
+echo " Push completed successfully! "
