f35e2828711c4d1cd7baffcaea8670fef41f8a7b
hrishi <hrirakes@gmail.com>
2026-03-10 22:58:30 +0530

    bugfixv13

--- UI/widgets.py
+++ UI/widgets.py
@@ -1,3 +1,6 @@
 from .Qt6widgets.thinkingblock import *
 from .Qt6widgets.messagewidget import *
 from .Qt6widgets.refrencepanels import *
+from .Qt6widgets.chatmodule import *
+from .Qt6widgets.chatarea import *
+from .Qt6widgets.inputbar import *
